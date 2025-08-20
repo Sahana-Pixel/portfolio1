93834f3f0bf5144f7ec432b358b59d9615c3f548
Sahana-pixel <sahanaacharya85@gmail.com>
2025-08-20 20:55:55 +0530

    added resume

--- src/components/hero/ActionButtons.tsx
+++ src/components/hero/ActionButtons.tsx
@@ -6,18 +6,15 @@ export function ActionButtons() {
   return (
 <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto md:mx-0 max-w-md">
       <a
-  href="/resume.pdf"
-  target="_blank" 
-  rel="noopener noreferrer"
-  className="flex justify-center items-center gap-2 px-4 py-2 rounded-md 
-             text-sm sm:text-base bg-pink-800 text-white hover:bg-pink-700 
-             transition-transform duration-300 transform hover:scale-105 
-             w-full sm:w-auto"
->
-  <FileText className="w-5 h-5" />
-  <span>View Resume</span>
-</a>
-
+        href=""
+        className="flex justify-center items-center gap-2 px-4 py-2 rounded-md 
+                   text-sm sm:text-base bg-pink-800 text-white hover:bg-pink-700 
+                   transition-transform duration-300 transform hover:scale-105 
+                   w-full sm:w-auto"
+      >
+        <FileText className="w-5 h-5" />
+        <span>View Resume</span>
+      </a>
 
       <Link
         href="#contact"
